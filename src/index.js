7dca9cb56ad750e471b139e89c426f9571f2b447
Bochin Anton <antonbocin342@gmail.com>
2024-08-12 18:58:46 +0300

    feat: did task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  unpackedMatrix = []
+  let k = -1
+  if (!matrix)
+    return unpackedMatrix
+  for (e of matrix){
+    k = k * (-1)
+    if (k === 1) {
+        for(number of e)
+            unpackedMatrix.push(number)
+        }
+    else {
+        newArr = e.reverse()
+        for(number of newArr)
+            unpackedMatrix.push(number)
+        }
+    }
+  return unpackedMatrix
 }
